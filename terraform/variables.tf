53ca4f0d0bd50b54fd161b1de382d8624556a7ca
fedosovdv <ferumdos@yandex.ru>
2022-07-06 21:12:43 +0500

    terraform-2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,22 +6,17 @@ variable folder_id {
 }
 variable zone {
   description = "Zone"
-  # Значение по умолчанию
-  default = "ru-central1-a"
-}
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
-}
-variable image_id {
-  description = "Disk image"
-}
-variable subnet_id {
-  description = "Subnet"
+  default     = "ru-central1-a"
 }
 variable service_account_key_file {
   description = "key .json"
 }
-variable private_key_path {
-  description = "ssh_key"
+variable bucket_name {
+  description = "bucket name"
+}
+variable access_key {
+  description = "key_id"
+}
+variable secret_key {
+  description = "secret"
 }
